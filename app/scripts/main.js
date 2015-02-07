bbb29db7ed0e9eb4d85dc550389ef4c9538a81bf
Jeremy Crawford <jcrawford.design@gmail.com>
2015-02-06 17:54:24 -0800

    changed background image and added menu content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+// Header Nav Dropdown 
+
+$(function(){
+
+    $("ul.dropdown li").hover(function(){
+    
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+    
+    }, function(){
+    
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+    
+    });
+    
+    $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});

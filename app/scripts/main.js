baa37b7151517407c1dfa0531799456f9f4aae50
Jeremy Crawford <jcrawford.design@gmail.com>
2015-02-07 19:52:42 -0800

    changed layout and design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,38 @@
-// Header Nav Dropdown 
 
-$(function(){
 
-    $("ul.dropdown li").hover(function(){
-    
-        $(this).addClass("hover");
-        $('ul:first',this).css('visibility', 'visible');
-    
-    }, function(){
-    
-        $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
-    
-    });
-    
-    $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+// Normal Clicks
 
+$(function() {
+  $('#header-contact-button').click(function() {
+    $('body').toggleClass('show-contact');
+     return false;
+  });
+  
 });
+
+$(function() {
+  $('#close-contact-button').click(function() {
+    $('body').toggleClass('show-contact');
+     return false;
+  });
+  
+});
+
+// $(function() {
+//   $('#exit-contact-container').click(function() {
+//     $('body').toggleClass('show-nav');
+//      return false;
+//   });
+  
+// });
+
+
+// Toggle with hitting of ESC
+// $(document).keyup(function(e) {
+//   if (e.keyCode == 27) {
+//    $('body').toggleClass('show-contact');
+//   }
+// });
+
+//Exit Buttons
+
